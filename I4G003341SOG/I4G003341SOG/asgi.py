bcf0e36cabdca09b2e7b5e19fd91381841094737
Olufunke <jaladesanwa044@stu.ui.edu.ng>
2022-06-24 09:46:28 +0100

    changes

--- I4G003341SOG/I4G003341SOG/asgi.py
+++ I4G003341SOG/I4G003341SOG/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for olufunke project.
+ASGI config for I4G003341SOG project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olufunke.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G003341SOG.settings')
 
 application = get_asgi_application()
--- I4G003341SOG/I4G003341SOG/urls.py
+++ I4G003341SOG/I4G003341SOG/urls.py
@@ -1,4 +1,4 @@
-"""olufunke URL Configuration
+"""I4G003341SOG URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
